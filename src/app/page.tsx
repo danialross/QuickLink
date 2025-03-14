092fab7e242e4e60c9c14ce0180314c569775fa0
danialr0ss <danial.ross@aspen-spectra.com>
2025-03-14 16:22:15 +0800

    fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { RiLinksLine } from "react-icons/ri";
-import { FormEvent, useEffect, useRef, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { MdContentCopy } from "react-icons/md";
-import { MdOutlineFileDownload } from "react-icons/md";
+import { MdOutlineFileDownload, MdClear } from "react-icons/md";
 
 export default function Home() {
     const [link, setLink] = useState<string>("");
@@ -22,7 +22,7 @@ export default function Home() {
             try {
                 const result = await fetch("/api/generate-link", {
                     method: "POST",
-                    body: formData
+                    body: formData,
                 });
                 if (!result.ok) {
                     console.log("Failed to generate link");
@@ -31,7 +31,6 @@ export default function Home() {
                 const { link } = await result.json();
                 console.log("Generated link", link);
                 setLink(link);
-
             } catch (error) {
                 console.log(error);
             }
@@ -39,11 +38,10 @@ export default function Home() {
     };
 
     const handleFileChange = () => {
-        if (inputRef.current && inputRef.current.files) {
+        if (inputRef.current && inputRef.current.files && inputRef.current.files[0]) {
             setCanUpload(true);
             return;
         }
-        setCanUpload(false);
     };
 
     const handleCopyText = () => {
@@ -54,25 +52,49 @@ export default function Home() {
         }
     };
 
+    const handleRemoveFile = () => {
+        if( inputRef.current && inputRef.current.files && inputRef.current.files[0]){
+            inputRef.current.value = "";
+            setCanUpload(false);
+            setLink("")
+        }
+    }
+
     return (
         <div
-            className={"w-screen h-screen flex flex-col items-center justify-center bg-primary text-secondary text-md sm:text-2xl font-kanit gap-4 p-4"}>
+            className={
+                "w-screen h-screen flex flex-col items-center justify-center bg-primary text-secondary text-base sm:text-2xl font-kanit gap-4 p-4"
+            }
+        >
             <div className={"flex items-center justify-center gap-4 text-6xl"}>
                 <RiLinksLine />
                 QuickLink
             </div>
-            <p className={"pb-8 text-center"}>Upload a file to generate a link that can be shared with others for easy
-                access to the
-                file</p>
+            <p className={"pb-8 text-center"}>
+                Upload a file to generate a link that can be shared with others
+                for easy access to the file
+            </p>
             <form onSubmit={uploadFile}>
-                <div className={"w-fit flex gap-4 pb-16"}>
-                    <input className={"w-full rounded-lg shadow-2xl border border-secondary p-4 sm:p-6"}
-                           type="file"
-                           ref={inputRef}
-                           onChange={handleFileChange}
-                    />
+                <div className={"w-fit flex gap-4 pb-8"}>
+                    <div
+                        className={
+                            "flex items-center w-full rounded-lg shadow-2xl border border-secondary text-base p-2 sm:p-6 gap-6"
+                        }
+                    >
+                        <input
+                            type="file"
+                            ref={inputRef}
+                            onChange={handleFileChange}
+                            className={`transition-all duration-300 ease-in-out w-full`}
+                        />
+                        {canUpload && (
+                            <button onClick={handleRemoveFile}>
+                                <MdClear size={25} className={"w-fit h-fit hover:scale-150 hover:text-accent transition-transform duration-300 ease-in-out"}/>
+                            </button>
+                        )}
+                    </div>
                     <button
-                        className={`${canUpload ? "bg-primary opacity-100" : "bg-accent opacity-50"} border border-secondary rounded-lg p-4 sm:p-6 shadow-2xl hover:bg-accent transition-colors duration-200 ease-in-out`}
+                        className={`${canUpload ? "bg-primary opacity-100" : "bg-accent opacity-50"} text-base border border-secondary rounded-lg p-4 sm:p-6 shadow-2xl hover:bg-accent transition-colors duration-200 ease-in-out`}
                         type="submit"
                         disabled={!canUpload}
                     >
@@ -82,22 +104,31 @@ export default function Home() {
             </form>
 
             <div
-                className={`w-full flex flex-col justify-center items-center transition-opacity duration-200 ease-in-out ${link ? "opacity-100" : "opacity-0"}`}>
+                className={`w-full flex flex-col justify-center items-center transition-opacity duration-200 ease-in-out ${link ? "opacity-100" : "opacity-0"}`}
+            >
                 <p className={"pb-4 text-center"}>File is Ready!</p>
                 <div
-                    className={` h-24 p-4 border border-secondary rounded-lg flex justify-center items-center shadow-2xl`}
+                    className={`p-2 rounded-lg flex justify-center items-center `}
                 >
-                    <div className={"w-20  flex justify-center items-center"}>
+                    <div className={"w-20 flex justify-center items-center"}>
                         <MdOutlineFileDownload
-                            className={"text-5xl text-secondary hover:text-6xl hover:text-accent  duration-200 ease-in-out"} />
+                            size={40}
+                            className={
+                                "text-secondary hover:scale-150 hover:text-accent transition-transform duration-200 ease-in-out"
+                            }
+                        />
                     </div>
                     <div className={"w-20  flex justify-center items-center"}>
                         <MdContentCopy
-                            className={"text-4xl text-secondary hover:text-5xl hover:text-accent  duration-200 ease-in-out"}
+                            size={35}
+                            className={
+                                "text-secondary hover:scale-150 hover:text-accent transition-transform duration-200 ease-in-out"
+                            }
                             onClick={handleCopyText}
                         />
                     </div>
                 </div>
             </div>
-        </div>);
+        </div>
+    );
 }
